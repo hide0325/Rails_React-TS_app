9fd086e219023e15e6ab392083f6aa47faf67a3d
hide0325 <tmnftc@yahoo.co.jp>
2022-12-19 18:47:29 +0900

    Appコンポーネントをレイアウト

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,5 +1,47 @@
 import { FC, useEffect } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 import axios from 'axios'
+import styled from 'styled-components'
+import AddTodo from 'components/AddTodo'
+import TodoList from 'components/TodoList'
+import EditTodo from 'components/EditTodo'
+
+const Navbar = styled.nav`
+  background: #dbfffe;
+  min-height: 8vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 23px;
+  letter-spacing: 3px;
+`
+
+const NavItems = styled.ul`
+  display: flex;
+  width: 400px;
+  max-width: 40%;
+  justify-content: space-around;
+  list-style: none;
+`
+
+const NavItem = styled.li`
+  font-size: 19px;
+  font-weight: bold;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+`
+
+const Wrapper = styled.div`
+  width: 700px;
+  max-width: 85%;
+  margin: 20px auto;
+`
 
 const App: FC = () => {
   useEffect(() => {
@@ -18,12 +60,24 @@ const App: FC = () => {
 
   return (
     <>
-      <header>
-        <p>ほげ</p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+        <Logo>TODO</Logo>
+        <NavItems>
+          <NavItem>
+            <Link to="/todos">Todos</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/todos/new">Add New Todo</Link>
+          </NavItem>
+        </NavItems>
+      </Navbar>
+      <Wrapper>
+        <Routes>
+          <Route path="/todos" element={<TodoList />} />
+          <Route path="/todos/new" element={<AddTodo />} />
+          <Route path="/todos/:id/edit" element={<EditTodo />} />
+        </Routes>
+      </Wrapper>
     </>
   )
 }
